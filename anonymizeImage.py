b0be72b1f73bc676c8bfc84178bb4afb9846785a
Daniel <Daniel.H.Iversen@sintef.no>
2015-12-08 15:27:59 +0100

    Added script functionality

--- anonymizeImage.py
+++ anonymizeImage.py
@@ -1,4 +1,6 @@
 from PIL import Image  
+import os
+import sys
 
 def readImage(filePathIn):
     try:
@@ -34,3 +36,27 @@ def anonymizeImage(picture, pixelsX=None, pixelsY=None, color=(0, 0, 0)):
                 continue
             pixels[x, y] = color
     return picture
+    
+    
+def _convertFolder(folderPathIn):
+    for root, dirs, files in os.walk(folderPathIn):
+        for file_i in files:
+            filePath = os.path.join(root, file_i)
+ 
+            imData = readImage(filePath)
+            if imData:
+                print "Image file: " +  os.path.basename(filePath)
+                filePathOut = filePath
+                try:
+                    imDataOut = anonymizeImage(imData, pixelsX=range(230, 1500), pixelsY=range(7, 43), color=(190, 192, 194))
+                    saveImage(filePathOut, imDataOut)
+                except Exception as e:
+                    print str(e)
+                    print "File: "+ filePath
+  
+print "----------------------------------------"
+if __name__ == '__main__':
+    
+    print sys.argv
+    if len(sys.argv)==2:
+        _convertFolder(sys.argv[1])
